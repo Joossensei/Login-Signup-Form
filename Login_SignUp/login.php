ad07cfabb621280f9af48ad6f0010d215c9d834b
Joost <40595903+Joossie010@users.noreply.github.com>
2020-12-03 20:44:48 +0100

    Added the files
    Added all the files for the working login system

---
+++ Login_SignUp/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../cs/style.css">
+    <title>Inloggen</title>
+</head>
+<body>
+
+    <form method="POST" action="login.inc.php">
+	  <fieldset>
+                <legend>In Loggen!</legend>
+		<div class="inputmenu">
+                <input type="text" name="mailuid" class="inputtext" placeholder="E-mail/Username">
+                <input type="password" name="pwd" class="inputtext" placeholder="Password">
+                <button type="submit" name="login-submit">Login</button>
+			<p style="font-size: 12px">Nieuw Account, <a href="signup.php" value="signup">Klik hier</a></p>
+			</div>
+		</fieldset>
+    </form>
+	
+
+</body>
+</html>
---
+++ Login_SignUp/signup.php
@@ -0,0 +1,59 @@
+<?php
+
+$succes = $_GET['signup'];
+$error = $_GET['error'];
+
+if($succes == "succes"){
+	$message = "U heeft uw account aan gemaakt!";
+}
+
+elseif($error == "emptyfields") {
+	$message = "Er is een leeg veld!";
+}
+
+elseif($error == "invalidmailuid"){
+	$message = "Onjuiste gebruikersnaam/Mail!";
+}
+elseif($error == "invalidmail"){
+	$message = "Onjuiste Mail!";
+}
+elseif($error == "invaliduid"){
+	$message = "Onjuiste gebruikersnaam!";
+}
+elseif($error == "passwordcheck"){
+	$message = "Wachtwoorden komen niet overeen!";
+}
+elseif($error == "sqlerror"){
+	$message = "Er gaat iets fout (╯°□°）╯︵ ┻━┻ dit komt niet door u!";
+}
+elseif($error == "usertaken"){
+	$message = "Gebruikersnaam is al in gebruik!";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../js/accountcreation.js"></script>
+    <link rel="stylesheet" href="../cs/style.css">
+    <title>Maak een account aan</title>
+</head>
+<body>
+    <form class="form-signup" action="signup.inc.php" method="post">
+		<fieldset>
+			<legend>Account Aanmaken!</legend>
+			<div class="inputmenu">
+        		<input type="text" name="uid" class="inputtext" placeholder="Username">
+        		<input type="text" name="mail" class="inputtext" placeholder="E-Mail">
+        		<input type="password" name="pwd" class="inputtext" placeholder="Password">
+        		<input type="password" name="pwd-repeat" class="inputtext" placeholder="Repeat Password">
+        		<button type="submit" name="signup-submit">Maak een Account aan!</button>
+			</div>
+			<p><?php 
+				$message
+				?></p>
+		</fieldset>
+    </form>
+</body>
+</html>
